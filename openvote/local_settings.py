b11e39ac7928decf19cbb900c1028df4eaa0ddf5
Dan Von Kohorn <dan@vonkohorn.com>
2012-08-04 03:14:57 -0400

    Fixed local settings

--- openvote/local_settings.py
+++ openvote/local_settings.py
@@ -1,4 +1,4 @@
-# Django settings for lame project.
+# Django settings for openvote project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -97,10 +97,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'lame.urls'
+ROOT_URLCONF = 'openvote.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'lame.wsgi.application'
+WSGI_APPLICATION = 'openvote.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
