ebf097425f1bbe775b20c067436ae95a42debf2c
Doug von Kohorn <dougvk@gmail.com>
2012-08-11 13:55:00 -0400

    added backends to client side app

--- static/js/desktop.js
+++ static/js/desktop.js
@@ -10,6 +10,7 @@ require.config({
       underscore: "libs/lodash-0.4.2",
       backbone: "libs/backbone-0.9.2",
       events: "libs/backbone-eventbroker.amd",
+      io: "libs/socket.io",
 
       // Require.js Plugins
       text: "plugins/text-2.0.0"
@@ -22,6 +23,10 @@ require.config({
       "backbone": {
           deps: ["underscore", "jquery"],
           exports: "Backbone"  //attaches "Backbone" to the window object
+      },
+      "io": {
+          deps: ["events", "backbone", "underscore", "jquery"],
+          exports: "io"
       }
 
   } // end Shim Configuration
@@ -29,8 +34,20 @@ require.config({
 });
 
 // Include Desktop Specific JavaScript files here (or inside of your Desktop router)
-require(['modernizr','jquery','backbone','events','routers/desktopRouter'], function(Modernizr, $, Backbone, Events, Desktop) {
+require(['modernizr','jquery','backbone','events','routers/desktopRouter','io'], function(Modernizr, $, Backbone, Events, Desktop, io) {
 
     // Instantiates a new Router
     this.router = new Desktop();
+
+    // Instantiates a new backend into window.socket
+    window.socket = io.connect('http://localhost:8080');
+    window.socket.on('connect', function () {
+        console.log('connected');
+    });
+    window.socket.on('message', function (msg) {
+        console.log(msg);
+    });
+    window.socket.on('disconnect', function () {
+        console.log('disconnected');
+    });
 });
