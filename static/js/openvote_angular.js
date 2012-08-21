296c2ec345cfb26281659fc6b20efe5ceab0cebe
Dan Von Kohorn <dan@vonkohorn.com>
2012-08-21 16:26:43 -0400

    latest working version -- switching to angularjs

--- static/js/openvote_angular.js
+++ static/js/openvote_angular.js
@@ -1,103 +1,60 @@
-angular.module('ovApp', ['ov_django']).
+angular.module('ovApp', []).
+    config(function($interpolateProvider) {
+        $interpolateProvider.startSymbol('<[');
+        $interpolateProvider.endSymbol(']>');
+    }).
     config(function($routeProvider) {
         $routeProvider.
-            when('/', {controller:ElectionListCtrl, templateUrl:'static/election_list.html'}).
-            when('/election/new', {controller:AddElectionCtrl, templateUrl:'static/election_edit.html'}).
-            when('/election/edit/:electionID', {controller:EditElectionCtrl, templateUrl:'static/election_edit.html'}).
-            when('/election/:electionId', {controller:ElectionCtrl, templateUrl:'static/election.html'}).
+            when('/', {controller:ElectionListCtrl, templateUrl:'static/templates/election_list.html'}).
+            when('/election/new', {controller:AddElectionCtrl, templateUrl:'static/templates/election_edit.html'}).
+            when('/election/edit/:electionID', {controller:EditElectionCtrl, templateUrl:'static/templates/election_edit.html'}).
+            when('/election/:electionId', {controller:ElectionCtrl, templateUrl:'static/templates/election.html'}).
             otherwise({redirectTo:'/'});
   });
- 
- 
-// Elections
 
-function ElectionListCtrl($scope, Election) {
-  // $scope.elections = Election.query();
-  $scope.elections = [
-    {name:'US Presidential Election 2012', voted:false},
-    {name:'Some Congressional Election', voted:false},
-    {name:'Another Congressional Election', voted:false}
-  ];
+function AppCtrl($scope) {
+    $scope.voter = {name:'Guest', is_authenticated:false};
 
-}
+    // $scope.elections = Election.query();
+    $scope.elections = [
+        {
+            name:'US Presidential Election 2012',
+            voted:false,
+            id:1
+        },
+        {
+            name:'Some Congressional Election',
+            voted:false,
+            id:2
+        },
+        {
+            name:'Another Congressional Election',
+            voted:false,
+            id:3
+        }
+    ];
+};
 
-function AddElectionCtrl($scope, $location, Election) {
-  $scope.save = function() {
-    Election.save($scope.election, function(election) {
-      $location.path('election/edit/' + project._id.$oid);
-    });
-  }
-}
- 
-function EditElectionCtrl($scope, $location, $routeParams, Election) {
-  var self = this;
- 
-  Election.get({id: $routeParams.electionId}, function(election) {
-    self.original = election;
-    $scope.election = new Election(self.original);
-  });
- 
-  $scope.isClean = function() {
-    return angular.equals(self.original, $scope.election);
-  }
- 
-  $scope.destroy = function() {
-    self.original.destroy(function() {
-      $location.path('/');
-    });
-  };
- 
-  $scope.save = function() {
-    $scope.election.update(function() {
-      $location.path('election/' + election._id.$oid);
-    });
-  };
-}
- 
-function ElectionCtrl($scope) {
- 
-  $scope.addElection = function() {
-    $scope.election = { name:$scope.electionName,
-                        voted:false};
-    $scope.elections.push($scope.election);
-    $scope.electionName = '';
-  };
- 
-  // count of elections the voter has voted in
-  $scope.elections_voted = function() {
-    var count = 0;
-    angular.forEach($scope.elections, function(election) {
-      count += election.voted ? 1 : 0;
-    });
-    return count;
-  };
+function ElectionListCtrl($scope) {
+    $scope.something = '1';
+};
 
-  // count of elections the voter has NOT voted in
-  $scope.elections_unvoted = function() {
-    var count = 0;
-    angular.forEach($scope.elections, function(election) {
-      count += election.voted ? 0 : 1;
-    });
-    return count;
-  };
- 
-  // refreshes elections list, purging those that have been voted.
-  $scope.unvoted_elections = function() {
-    var oldElections = $scope.elections;
-    $scope.elections = [];
-    angular.forEach(oldElections, function(election) {
-      if (!election.voted) $scope.elections.push(election);
-    });
-  };
-}
+function AddElectionCtrl($scope) {
+    $scope.something = '2';
+};
 
- 
-// Voter 
-  
+function EditElectionCtrl($scope, $location, $routeParams) {
+    $scope.election = {name:'todo - get election by ID', voted:false, id:$routeParams.electionId};
+};
+
+function ElectionCtrl($scope, $location, $routeParams) {
+    $scope.election = {name:'todo - get election by ID', voted:false, id:$routeParams.electionId};
+};
+
 function VoterCtrl($scope) {
-  $scope.voter = {name:'Guest', is_authenticated:false};
- 
-  $scope.authVoter = function() {
-    $scope.voter = {name:$scope.voterName, is_authenticated:true};
-  };
+    $scope.voter = {name:'Guest', is_authenticated:false};
+
+    $scope.authVoter = function() {
+        $scope.voter = {name:$scope.voterName, is_authenticated:true};
+    };
 }
--- static/js/openvote_django_api.js
+++ static/js/openvote_django_api.js
@@ -1,23 +1,23 @@
 
-// A module for conncting to the OpenVote.US API
+// A module for connecting to the OpenVote.US API
 
 angular.module('ov_django', ['ngResource']).
     factory('Election', function($resource) {
-      var Election = $resource('https://openvote.us/api/v1/elections/:id',
+      var Election = $resource('https://openvote.us/api/v1/election/:id/?format=json',
           { apiKey: 'blerg' }, {
             update: { method: 'PUT' }
           }
       );
  
-      Project.prototype.update = function(cb) {
-        return Project.update({id: this._id.$oid},
+      Election.prototype.update = function(cb) {
+        return Election.update({id: this._id.$oid},
             angular.extend({}, this, {_id:undefined}), cb);
       };
  
-      Project.prototype.destroy = function(cb) {
-        return Project.remove({id: this._id.$oid}, cb);
+      Election.prototype.destroy = function(cb) {
+        return Election.remove({id: this._id.$oid}, cb);
       };
  
-      return Project;
+      return Election;
     });
     
