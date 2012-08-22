e7b0a986e0624bc7c8f5aef4964d7feb0af2124b
Doug von Kohorn <dougvk@gmail.com>
2012-08-21 20:01:17 -0400

    initial candidates working

--- rest/tasks.py
+++ rest/tasks.py
@@ -1,5 +1,23 @@
 from celery import task
+import os
+import redis
+
+from django.core import serializers
+
+from openvote.models import Voter
+
+r_host = os.environ['REDIS_HOST']
+r_port = os.environ['REDIS_PORT']
+r_password = os.environ['REDIS_PASSWORD']
+r = redis.StrictRedis(host=r_host, port=int(r_port), db=0, password=r_password)
 
 @task()
 def add(x, y):
     return x + y
+
+@task()
+def get_voters(*args, **kwargs):
+    api_response = serializers.serialize('json',
+                        Voter.objects.all())
+    kwargs['api_response'] = api_response
+    r.publish(channel="default", message=api_response)
--- static/js/openvote_angular.js
+++ static/js/openvote_angular.js
@@ -2,14 +2,15 @@ angular.module('ovApp', []).
     config(function($interpolateProvider) {
         $interpolateProvider.startSymbol('<[');
         $interpolateProvider.endSymbol(']>');
-    }).
-    config(function($routeProvider) {
-        $routeProvider.
-            when('/', {controller:ElectionListCtrl, templateUrl:'static/templates/election_list.html'}).
-            when('/election/new', {controller:AddElectionCtrl, templateUrl:'static/templates/election_edit.html'}).
-            when('/election/edit/:electionID', {controller:EditElectionCtrl, templateUrl:'static/templates/election_edit.html'}).
-            when('/election/:electionId', {controller:ElectionCtrl, templateUrl:'static/templates/election.html'}).
-            otherwise({redirectTo:'/'});
+    })
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {controller:ElectionListCtrl, templateUrl:'static/templates/election_list.html'})
+            .when('/election/new', {controller:AddElectionCtrl, templateUrl:'static/templates/election_edit.html'})
+            .when('/election/edit/:electionID', {controller:EditElectionCtrl, templateUrl:'static/templates/election_edit.html'})
+            .when('/election/:electionId', {controller:ElectionCtrl, templateUrl:'static/templates/election.html'})
+            .when('/candidate/:candidateID', {controller:CandidateCtrl, templateUrl:'static/templates/candidate.html'})
+            .otherwise({redirectTo:'/'});
   });
 
 function AppCtrl($scope) {
@@ -20,36 +21,125 @@ function AppCtrl($scope) {
         {
             name:'US Presidential Election 2012',
             voted:false,
+            candidates : [
+                {
+                    name: 'Pres Cand 1',
+                    desc: 'This is why I should be Prezzy',
+                    htmlslug: 'This should be put in an iframe?',
+                    approved: true,
+                    id: 10
+                },
+                {
+                    name: 'Pres Cand 2',
+                    desc: 'This is why I should be Prezzy',
+                    htmlslug: 'This should be put in an iframe?',
+                    approved: false,
+                    id: 11
+                },
+                {
+                    name: 'Pres Cand 3',
+                    desc: 'This is why I should be Prezzy',
+                    htmlslug: 'This should be put in an iframe?',
+                    approved: false,
+                    id: 12
+                }
+            ],
             id:1
         },
         {
             name:'Some Congressional Election',
             voted:false,
+            candidates : [
+                {
+                    name: 'Cong Cand 1',
+                    desc: 'This is why I should be Congy',
+                    htmlslug: 'This should be put in an iframe?',
+                    approved: false,
+                    id: 4
+                },
+                {
+                    name: 'Cong Cand 2',
+                    desc: 'This is why I should be Congy',
+                    htmlslug: 'This should be put in an iframe?',
+                    approved: true,
+                    id: 5
+                },
+                {
+                    name: 'Cong Cand 3',
+                    desc: 'This is why I should be Congy',
+                    htmlslug: 'This should be put in an iframe?',
+                    approved: false,
+                    id: 6
+                }
+            ],
             id:2
         },
         {
             name:'Another Congressional Election',
             voted:false,
+            candidates : [
+                {
+                    name: 'AnCong Cand 1',
+                    desc: 'This is why I should be ANOTHER CONGY',
+                    htmlslug: 'This should be put in an iframe?',
+                    approved: false,
+                    id: 7
+                },
+                {
+                    name: 'AnCong Cand 2',
+                    desc: 'This is why I should be ANOTHER CONGY',
+                    htmlslug: 'This should be put in an iframe?',
+                    approved: false,
+                    id: 8
+                },
+                {
+                    name: 'AnCong Cand 3',
+                    desc: 'This is why I should be ANOTHER CONGY',
+                    htmlslug: 'This should be put in an iframe?',
+                    approved: true,
+                    id: 9
+                }
+            ],
             id:3
         }
     ];
-};
+    $scope.elections_voted = function () {
+        return 3;
+    };
+
+    $scope.elections_unvoted = function () {
+        return 0;
+    };
+}
 
 function ElectionListCtrl($scope) {
     $scope.something = '1';
-};
+}
 
 function AddElectionCtrl($scope) {
     $scope.something = '2';
-};
+}
 
 function EditElectionCtrl($scope, $location, $routeParams) {
     $scope.election = {name:'todo - get election by ID', voted:false, id:$routeParams.electionId};
-};
+}
 
 function ElectionCtrl($scope, $location, $routeParams) {
     $scope.election = {name:'todo - get election by ID', voted:false, id:$routeParams.electionId};
-};
+}
+
+function CandidateCtrl($scope, $location, $routeParams) {
+    $scope.candidate = _.filter(
+        _.map($scope.elections,
+              function (election) {
+                  var candidate  = _.find(election.candidates, function (candidate) { return candidate.id == $routeParams.candidateID; });
+                  if (candidate) { return candidate; }
+              }),
+        function (candidate) { 
+            if (candidate) { return candidate; }
+        }
+    )[0];
+}
 
 function VoterCtrl($scope) {
     $scope.voter = {name:'Guest', is_authenticated:false};
